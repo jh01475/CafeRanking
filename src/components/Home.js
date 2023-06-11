f45ec9811cd26955ab1513579cfc6103ac125209
jh0147 <jh0147@hotmail.com>
2023-06-11 23:37:25 +0900

    메인 페이지 구현 작업

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import '../css/Home.css'
 
 function Home() {
     return(
-        <div className="home">홈 화면입니다!!</div>
+        <div className="home">홈 화면입니다!!</div> 
     )
 }
 export default Home;
