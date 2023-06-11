8aad32e005b2ac5145f507da0ea4f4a4ca7aa243
jh0147 <jh0147@hotmail.com>
2023-06-11 23:35:18 +0900

    웹 페이지 이름 표시 작업

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
     return (
         <div className='container'>
             <div className="header">
-                <h1><div><Link to='/'>네이버 카페 게시물 랭킹 사이트</Link></div></h1>
+                <h1><div><Link to='/'>네이버 카페 게시물 랭킹 사이트</Link></div></h1> 
             </div>
         </div>
     )
